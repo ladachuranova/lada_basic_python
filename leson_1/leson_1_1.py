ad311035a25c177ef3a43f5dc9840eeb148dc00c
lyalya <lalachuranova@gmail.com->
2025-08-25 19:54:30 +0300

    lesson 1

--- leson_1/leson_1_1.py
+++ leson_1/leson_1_1.py
@@ -1,1 +1,28 @@
-print("Привет мир")
+# a = 7
+# b = a
+# print(b)
+# str_1 = "лугко в IT"
+# print(a)
+# print(str_1)
+# b = 8.34
+
+
+# a, b = 8, 10
+# a,b = b, a
+# print(a)
+# print(b)
+
+
+# print(type(a))
+# print(type(str_1))
+# print(type(b))
+
+
+a = c =  4
+b, d  = "Строка", "строка"
+
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
+
