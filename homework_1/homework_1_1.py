b004291e1800de59e74f2642defa71e4fdf6e3c2
lyalya <lalachuranova@gmail.com->
2025-08-26 09:47:28 +0300

    lesson 1

---
+++ homework_1/homework_1_1.py
@@ -0,0 +1,25 @@
+"""Задание 1"""
+name = "Lada"
+age = 20
+height = 1.65
+print(f"name: {name}, age: {age}, height: {height}")
+
+"""Задание 2"""
+x = 10
+print(type(x))
+x = 25,5
+print(type(x))
+x = "Python"
+print(type(x))
+
+
+"""Задание 3"""
+a = 7
+b = a
+print(a)
+a = 10
+print(a)
+print(b)
+
+
+x = y =
