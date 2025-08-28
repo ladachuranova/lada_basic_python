a67611727ea53a18505b74de1bebc09d661bdd3d
lyalya <lalachuranova@gmail.com->
2025-08-28 12:02:13 +0300

    homework 2

---
+++ homework_1/homework_2.py
@@ -0,0 +1,109 @@
+"""1"""
+a = 8
+b = 3
+x = 2.5
+y = -1,7
+print(type(a))
+print(type(b))
+print(type(x))
+print(type(y))
+
+
+"""2"""
+num1 = 4
+num2 = 6
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1 ** num2)
+
+
+"""3"""
+a = 10
+b = 3
+print(a / b)
+print(a // b)
+print(a % b)
+
+a1 = -10
+b = 3
+print(a / b)
+print(a // b)
+print(a % b)
+
+a2 = 10
+b2 = -3
+print(a / b)
+print(a // b)
+print(a % b)
+
+
+"""4"""
+print(5 + 3 * 2)
+print((5 + 3) * 2)
+print(10 / 2 ** 2)
+
+
+"""5"""
+count = 10
+print(count + 5 )
+print(count - 3)
+print(count * 2)
+print(count / 4)
+
+
+
+"""1"""
+s1 = "Python"
+s2 = ' Программирование'
+print(s1 +  s2)
+empty = ""
+print(len(empty))
+
+
+"""2"""
+first_name = "Иван"
+last_name = " Петров"
+print(first_name + last_name)
+
+
+"""3"""
+s = "Возраст: "
+age = 25
+print(s, str(age))
+
+x = "xa"
+print(x * 5)
+# print(x * 2.5) # Ошибка
+
+
+"""5"""
+text = "Привет, мир!"
+print(len(text))
+text1 = ""
+print(len(text1))
+
+
+"""6"""
+sentence = "Я изучаю Python"
+print("Python" in sentence)
+print("Java" in sentence)
+
+
+"""7"""
+a = "apple"
+b = "banana"
+print(a == b)
+print(a > b)
+print(a < b)
+print(a <= b)
+print(a >= b)
+print(a != b)
+
+
+"""8"""
+print(ord('A'))
+print(ord('a'))
+print(ord('я'))
